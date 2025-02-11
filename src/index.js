546780fdee885e111f9340c7c70e22f32f204614
Jan <jas.gol@poczta.onet.pl>
2025-02-11 17:14:57 +0100

    remove some random testing in index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,3 @@ logic.createTodo(["title","description","date","priority", true])
 console.log(logic.projects[0].tasks[0])
 logic.changeTodoCompletion(0)
 console.log(logic.projects[0].tasks[0])
-
-function a(){
-    b("123");
-}
-
-function b(text){
-    console.log(text);
-}
