59149eae766712c5437705dfed723ab7c6b66b49
Jan <jas.gol@poczta.onet.pl>
2025-02-12 11:44:14 +0100

    link DOMController and divide website into project and task sections

--- src/DOMController.js
+++ src/DOMController.js
@@ -7,4 +7,20 @@ import createAddButton from "./DOM/addButton";
 
 import todoLogic from "./todoLogic";
 
-export default function createDOMController(){}
+export default function createDOMController(){
+    const container = document.getElementById("container");
+    const mainContainer = document.createElement("div");
+    mainContainer.setAttribute("class","main-container");
+    //window["functionName"](arguments);
+
+    const projectsContainer = document.createElement("div");
+    projectsContainer.setAttribute("class","projects-container");
+
+    const tasksContainer = document.createElement("div");
+    tasksContainer.setAttribute("class","tasks-container");
+
+    mainContainer.appendChild(projectsContainer);
+    mainContainer.appendChild(tasksContainer);
+
+    container.appendChild(mainContainer);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import todoLogic from "./todoLogic.js";
+import createDOMController from "./DOMController.js";
 
 const logic = todoLogic();
 logic.addProject(["123","123"])
@@ -8,3 +9,5 @@ logic.createTodo(["title","description","date","priority", true])
 console.log(logic.projects[0].tasks[0])
 logic.changeTodoCompletion(0)
 console.log(logic.projects[0].tasks[0])
+
+const DOMController = createDOMController();
